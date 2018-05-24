0ff028e1217a606b72116d06c9728f4eed660122
Andrew Zabara <dr.chrom@gmail.com>
2018-05-24 21:48:08 +0300

    3.5.3. Customize the Catalog\Product\View\Description block, implement the _beforeToHtml() method, and set a custom description for the product here.

---
+++ app/code/Training/Test/Block/Product/View/Description.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Training\Test\Block\Product\View;
+
+class Description extends \Magento\Framework\View\Element\Template
+{
+    public function beforeToHtml(\Magento\Catalog\Block\Product\View\Description $originalBlock)
+    {
+        $originalBlock->getProduct()->setDescription('<pre>{Test custom description}</pre>');
+    }
+}
